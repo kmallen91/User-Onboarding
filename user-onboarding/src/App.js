1a7d83d5fe8977be727b074daaeab82c1ae8f213
Kevin Allen <kevin.allen91@gmail.com>
2019-09-19 13:01:27 -0700

    step 1 complete

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import FormikUserForm from "./components/Form"
+import UserForm from './components/Form'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <UserForm />
         
-      </header>
+      
     </div>
   );
 }
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -3,15 +3,60 @@ import {withFormik, Form, Field, setNestedObjectValues} from "formik"
 import * as Yup from "yup"
 
 
-const UserForm = () => {
+const UserForm = ({values}) => {
 
 
     return (
-        <div> TEST </div>
+        <div className="user-form">
+            <Form>
+                <label> Name:
+                    <Field
+                        type="text"
+                        name="name"
+                        placeholder="Name"
+                        />
+                </label>
+                <label> Email:
+                    <Field
+                        type="text"
+                        name="email"
+                        placeholder="Email"
+                        />
+                </label>
+                <label> Password:
+                    <Field
+                        type="password"
+                        name="password"
+                        placeholder=""
+                        />
+                </label>
+                <label> Terms of Service
+                    <Field
+                        type="checkbox"
+                        name="terms"
+                        checked={values.checkbox}
+                        
+                        />
+                </label>
+                <button className="button"> Submit </button>
+
+            
+            
+            </Form>
+        
+        </div>
     )
 }
 
 const FormikUserForm = withFormik({
+    mapPropsToValues({name, email, password, terms}) {
+        return {
+            name: name || "",
+            email: email || "",
+            password: password || "",
+            terms: terms || false,
+        }
+    }
 
 
 })(UserForm)
